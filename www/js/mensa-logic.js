7b3de0d4dc4b83b9d713280e6d3e058174fc02b8
Hendrik Geßner <HENGE01@gmail.com>
2013-11-18 18:31:27 +0100

    Reduced meal templating code, added switch for phonegap vs. web app and sorted meals according to anzeigeprio.

--- www/js/mensa-logic.js
+++ www/js/mensa-logic.js
@@ -33,7 +33,14 @@ $(document).on("pageinit", "#start", function () {
  */
 function loadMenu(location) {
     var d = Q.defer();
-    $.get("http://fossa.soft.cs.uni-potsdam.de:8280/services/mensaParserJSON/readCurrentMeals?location=" + location).done(d.resolve).fail(d.reject);
+    var url = "http://fossa.soft.cs.uni-potsdam.de:8280/services/mensaParserJSON";
+
+    // If we are not in an app environment, we have to use the local proxy
+    if (typeof app === 'undefined') {
+        url = "/fossa-services/mensaParserJSON";
+    }
+
+    $.get(url + "/readCurrentMeals?location=" + location).done(d.resolve).fail(d.reject);
     return d.promise;
 }
 
@@ -73,19 +80,23 @@ function mapToDay(icons) {
         var dayData = {};
         dayData.title = new Date(day.key).toLocaleDateString();
         dayData.contentId = _.uniqueId("id_");
-        dayData.meals = _.map(day.value.item, mapToMeal(icons));
+        dayData.meals = _.chain(day.value.item).sortBy(sortByAnzeigeprio).map(mapToMeal(icons)).value();
         return dayData;
     }
+}
+
+function sortByAnzeigeprio(element) {
+    return element.anzeigeprio;
 }
 
 function mapToMeal(icons) {
     return function (meal) {
         var mealData = {};
         mealData.title = meal.titel;
-        mealData.description= meal.beschreibung;
-        mealData.prices= meal.preise.entry[0].value + "€ / " + meal.preise.entry[1].value + "€ / " + meal.preise.entry[2].value + "€";
+        mealData.description = meal.beschreibung;
+        mealData.prices = meal.preise.entry[0].value + "€ / " + meal.preise.entry[1].value + "€ / " + meal.preise.entry[2].value + "€";
 
-        mealData.ingredients=[];
+        mealData.ingredients = [];
         if ($.isArray(meal.essenstyp)) {
             for (var typIndex in meal.essenstyp) {
                 mealData.ingredients.push(icons[meal.essenstyp[typIndex]]);
@@ -113,8 +124,8 @@ function prepareMeals(meals, icons) {
  * @returns {Array|*|j.map}
  */
 function filterEmptyMeals(days) {
-    return _.map(days, function(day){
-        day.meals = _.filter(day.meals, function(meal){
+    return _.map(days, function (day) {
+        day.meals = _.filter(day.meals, function (meal) {
             return meal.description != "";
         });
         return day;
@@ -122,47 +133,18 @@ function filterEmptyMeals(days) {
 }
 
 function drawMeals(days) {
-    var dayTemplate = $("#date-template").clone().html();
+    var dayTemplate = $("#meal-template").clone().html();
     var createDay = _.template(dayTemplate);
 
-    var mealTemplate = $("#meal-template").clone().html();
-    var createMeal = _.template(mealTemplate);
-
-    // Create html
-    for (var dayIndex in days) {
-        var dayData = days[dayIndex];
-
-        // Add day section to html
+    // Add day section to html
+    _.each(days, function (dayData) {
         var htmlDay = createDay(dayData);
         $("#todaysMenu").append(htmlDay);
-
-        for (var mealIndex in dayData.meals) {
-            var mealData = dayData.meals[mealIndex];
-
-            var htmlMeal = createMeal(mealData);
-            $("#" + dayData.contentId).append(htmlMeal);
-        }
-    }
+    })
 
     // Tell collapsable set to refresh itself
     $("#todaysMenu").collapsibleset("refresh");
-}
 
-/**
- * Loads all meals for a given mensa and date.
- * @param date
- * @param location One of the values ["Griebnitzsee", "NeuesPalais", "Golm"]
- * @param onSuccess
- * @param onError
- */
-function loadCurrentMeals(date, location, onSuccess, onError) {
-    var ws = new mensaParser_provider_elis_unipotsdam_de__MensaParser();
-    ws.url = "http://localhost/fossa/mensaParser-1.0/ws?wsdl";
-    ws.readCurrentMeals(function (meals) {
-        // Filter for given date
-        var allMeals = meals.getReturn();
-        alert(allMeals);
-    }, function (httpStatus, httpStatusText) {
-        onError();
-    }, location);
+    // Open the first section
+    $("#" + days[0].contentId).trigger('expand');
 }
